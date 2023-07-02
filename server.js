f20af6eeb850b0299605944403dee079299f5e0c
Lasith Ranahewa <lasithmrana@gmail.com>
2023-07-02 12:47:26 +0530

    add responses

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
-  res.send('BMI Calculator')
+    res.sendFile(__dirname + '/bmiCalculator.html')
+})
+
+app.post('/', function (req, res) {
+    let weight = Number(req.body.weight)
+    let height = Number(req.body.height)
+    let bmi = weight / (height * height)
+    res.send('Your BMI is ' + bmi)
 })
 
 app.listen(3000)
